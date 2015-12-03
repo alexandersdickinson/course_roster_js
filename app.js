8f3f1691599e737bf6987282b32ec136b99584b1
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-12-02 17:21:49 -0800

    Something wrong with StudentsCtrl.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-var CourseRoster = angular.module('courseRoster', ['ui.router']);
+var courseRoster = angular.module('courseRoster', ['ui.router']);
+
+courseRoster.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider.state('home', {
+		url: "",
+		templateUrl: "partials/home.html"
+	});
+	
+	$stateProvider.state('courses', {
+		url: "/courses", 
+		templateUrl: "partials/courses.html",
+		controller: "CoursesCtrl"
+	});
+	
+	$stateProvider.state('courses.students', {
+		url: "/:courseId",
+		templateUrl: "partials/courses.students.html",
+		controller: "StudentsCtrl"
+	});
+});
